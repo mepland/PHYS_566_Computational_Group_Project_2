fb8ccdaf38307a8d87477a511e206eb31a546e44
Wesley Cohen <wesley.cohen2@duke.edu>
2016-04-04 20:30:02 -0400

    Added Function to Input Different Lattice Sizes

--- code/Project2_Code.py
+++ code/Project2_Code.py
@@ -45,8 +45,8 @@ def inBounds(x,y,N):
         inCheck=True
     return inCheck
    
-#Create a function to check if an individual lattice point is a neighbor to the point of interests.
-#Returns true if the point is a neighbor and false if the point is not a neighbor.
+#Create a function to check if an individual lattice point is an occupied neighbor to the point of interests.
+#Returns true if the point is a neighbor and false if the point is not an occupied neighbor.
 def checkNeighbor(x,y,N):
     neighbor=True
     if N[x,y]!=0:
@@ -63,94 +63,91 @@ def isIdentical(testList):
         if first!=testList[i]:
             return False
     return True   
+
+#Create a function to take in different lattice sizes 
+#This is the biggest function in the program
+#N is the lattice size
+
+N=5
+L=np.zeros((N,N))
+
+def findCluster(N):
+    m=50                               #Number of Simulations
+    random.seed()                      #Initialize Random Number Generator
     
-#Set the Lattice Size
-N_5=5
+    #Start Values-One Value for the x-direction (row number) another for the y-diretion(column number)
+    S_x=random.randrange(0,N)
+    S_y=random.randrange(0,N)
 
-m=50                               #Number of Simulations
-random.seed()                      #Initialize Random Number Generator
+    #Create the lattice using the N dimension
+    L=np.zeros((N,N))
 
+    #Set The Initial Random Occupied Site
+    L[S_x,S_y]=1
 
-#Start Values-One Value for the x-direction another for the y-diretion
-S_5x=random.randrange(0,N_5)
-S_5y=random.randrange(0,N_5)
+    #Keep track of the number of clusters
+    clusterCount=1
 
-#Create Each Size Lattice
-L_5=np.zeros((N_5,N_5))
-
-#Set The Initial Random Occupied Site
-L_5[S_5x,S_5y]=1
-
-#Keep track of the number of clusters
-clusterCount=1
+    #Generate a New Random Number
+    newx=random.randrange(0,N)
+    newy=random.randrange(0,N)
 
 
-#Generate a New Random Number
-newx=random.randrange(0,N_5)
-newy=random.randrange(0,N_5)
+    #Check if Each of the Neighbors is Inside the Lattice
+    left=inBounds(newx-1,newy,N)
+    right=inBounds(newx+1,newy,N)
+    top=inBounds(newx,newy-1,N)
+    bottom=inBounds(newx,newy+1,N)
 
+    #Initialize these variables which determine whether the neighbors are occupied
+    leftNeigh=False
+    rightNeigh=False
+    topNeigh=False
+    bottomNeigh=False
 
-#Check if Each of the Neighbors is Inside the Lattice
-left=inBounds(newx-1,newy,N_5)
-right=inBounds(newx+1,newy,N_5)
-top=inBounds(newx,newy-1,N_5)
-bottom=inBounds(newx,newy+1,N_5)
+    #Check if the neighbors are occupied
+    if left==True:
+        leftNeigh=checkNeighbor(newx-1,newy,L)
+    if right==True:
+        rightNeigh=checkNeighbor(newx+1,newy,L)
+    if top==True:
+        topNeigh=checkNeighbor(newx,newy-1,L)
+    if bottom==True:
+        bottomNeigh=checkNeighbor(newx,newy+1,L)
 
-#Initialize these variables which determine whether the neighbors are occupied
-leftNeigh=False
-rightNeigh=False
-topNeigh=False
-bottomNeigh=False
-
-#Check if the neighbors are occupied
-if left==True:
-    leftNeigh=checkNeighbor(newx-1,newy,L_5)
-if right==True:
-    rightNeigh=checkNeighbor(newx+1,newy,L_5)
-if top==True:
-    topNeigh=checkNeighbor(newx,newy-1,L_5)
-if bottom==True:
-    bottomNeigh=checkNeighbor(newx,newy+1,L_5)
-
-#Case 1, No Neighbors are Occupied, Make a New Cluster
-if leftNeigh==False and rightNeigh==False and topNeigh==False and bottomNeigh==False:
-    L_5[newx,newy]=clusterCount+1   #Make a new cluster
-    clusterCount=clusterCount+1     #Increase the total number of clusters by 1
+    #Case 1, No Neighbors are Occupied, Make a New Cluster
+    if leftNeigh==False and rightNeigh==False and topNeigh==False and bottomNeigh==False:
+        L[newx,newy]=clusterCount+1   #Make a new cluster
+        clusterCount=clusterCount+1     #Increase the total number of clusters by 1
     
-#Case 2, One Common Neighbor, Add to an Existing Cluster
-#Check if each neighbor is occupied using if statements
-elif leftNeigh==True or rightNeigh==True or topNeigh==True or bottomNeigh==True:
-    neighCount=0                 #The number of total occupied neighbors
-    occupyNeigh=[0.0]*4           #An array with all the occupied neighbors
-    if leftNeigh==True:
-        occupyNeigh[neighCount]=L_5[newx-1,newy]
-        neighCount=neighCount+1
-    if rightNeigh==True:
-        occupyNeigh[neighCount]=L_5[newx+1,newy]
-        neighCount=neighCount+1
-    if topNeigh==True:
-        occupyNeigh[neighCount]=L_5[newx,newy-1]
-        neighCount=neighCount+1
-    if bottomNeigh==True:
-        occupyNeigh[neighCount]=L_5[newx,newy-1]
-        neighCount=neighCount+1
+    #Case 2, One Common Neighbor, Add to an Existing Cluster
+    #Check if each neighbor is occupied using if statements
+    elif leftNeigh==True or rightNeigh==True or topNeigh==True or bottomNeigh==True:
+        neighCount=0                 #The number of total occupied neighbors
+        occupyNeigh=[0.0]*4           #An array with all the occupied neighbors
+        if leftNeigh==True:
+            occupyNeigh[neighCount]=L[newx-1,newy]
+            neighCount=neighCount+1
+        if rightNeigh==True:
+            occupyNeigh[neighCount]=L[newx+1,newy]
+            neighCount=neighCount+1
+        if topNeigh==True:
+            occupyNeigh[neighCount]=L[newx,newy-1]
+            neighCount=neighCount+1
+        if bottomNeigh==True:
+            occupyNeigh[neighCount]=L[newx,newy-1]
+            neighCount=neighCount+1
             
-    occupyNeigh=occupyNeigh[0:neighCount]      #Resize the array to the number of occpuied neighbors
-    sameNeigh=isIdentical(occupyNeigh)         #Use isIdentical to determine if all occupied neighbors are occupied by the same number
+        occupyNeigh=occupyNeigh[0:neighCount]      #Resize the array to the number of occpuied neighbors
+        sameNeigh=isIdentical(occupyNeigh)         #Use isIdentical to determine if all occupied neighbors are occupied by the same number
      
-    #If the occupied neighbors are all the same, assign the current point the same value
-    if sameNeigh==True:
-        L_5[newx-1,newy]=occupyNeigh[0]
+        #If the occupied neighbors are all the same, assign the current point the same value
+        if sameNeigh==True:
+            L[newx-1,newy]=occupyNeigh[0]
     
 #Case 3, Multiple Common Neighbors, Merge Clusters
 
 
-
-    
-    
-
-
-
-
-    
-
+    return L             #The function findCluster returns the lattice with cluster 
+#Find the cluster for different lattice sizes
+N_5=findCluster(5)
