4f3f0cabf1858a17bcee5c447e3c9953ec4be98c
Yuanyuan Xu <yx82@duke.edu>
2016-04-06 22:53:45 -0400

    Add data for part b

--- code/main.py
+++ code/main.py
@@ -13,37 +13,51 @@ import numpy as np
 ############################# Test ##############################
 # real-time plotting
 if False:
-    p, grid = percolate(N=50, show=True)
+    pc, grid = percolate(N=20, show=False)
+    plt.close()
     # plot the figure at p = pc
     plt.imshow(grid, origin='lower', interpolation='nearest')
+    print pc
     plt.show()
 
 ############################ Part a #############################
-if True:
+if False:
     arr_N = [5, 10, 15, 20, 30, 50, 80]
     ave_pc = []
+    if False:
+        for i in range(len(arr_N)):
+            print i
+            sum = 0.0
+            for j in range(50):
+                p, grid = percolate(N=arr_N[i], show=False)
+                sum += p
+            ave_pc.append(sum / 50.0)
+        np.save('../output/part a/pc', ave_pc)
 
-    for i in range(len(arr_N)):
-        print i
-        sum = 0.0
-        for j in range(50):
-            p, grid = percolate(N=arr_N[i], show=False)
-            sum += p
-        ave_pc.append(sum / 50.0)
-
-    np.save('../output/pc', ave_pc)
-
-    ave_pc = np.load('../output/pc.npy')
+    ave_pc = np.load('../output/part a/pc.npy')  # load data
 
     x = [1.0 / float(k) for k in arr_N]
     plt.plot(x, ave_pc, marker='o')
     plt.show()
     # TODO curve-fitting
 
-##################### Part b #####################
-if False:
-    pc, p, frac, grid = percolate(N=100, fraction='True')
-    p -= pc
-    plt.plot(p, frac)
+############################ Part b #############################
+if True:
+    if False:
+        F_ave = np.zeros(100*100)
+        for i in range(100):
+            print i
+            pc, p, frac, grid = percolate(N=100, fraction='True')
+            p -= pc
+            if len(F_ave) <= len(p):
+                F_ave = F_ave + p[0: len(F_ave)]
+            else:
+                F_ave = F_ave[0: len(p)] + p
+        F_ave /= 100.0
+        np.save('../output/part b/F',F_ave)
+
+    F_ave = np.load('../output/part b/F.npy')
+
+    x = [float(i)/(100.0**2) for i in range(1, len(F_ave)+1)]
+    plt.loglog(x, F_ave)
     plt.show()
-    # TODO
